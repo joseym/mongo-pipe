9b3c27b6833e6d6f37756eae5bfdfe238228cb6a
Josey Morton <me@josey.io>
2016-08-17 14:09:14 -0500

    working on initial script

--- index.js
+++ index.js
@@ -38,40 +38,38 @@ let s3 = new AWS.S3({
 });
 
 
-const progress = require('progress-stream');
-
 let prog = progress({
   time: 100
 });
 
 let zlib = require('zlib');
 
-series([
-  // connect
-  (callback) => {
-    return connect(url, callback);
-  },
-  (callback) => {
-    return connect.addConnection(local, 'local', callback);
-  },
-  (callback) => {
-    return connect.addConnection(staging, 'staging', callback);
-  }
-], (err, results) => {
-
-  let backup = new (require('./dump'))('rezflow');
-  backup.on('end', function(){
-    console.log('done writing');
-  });
-  // s3.upload({ Body: backup.pipe(zlib.createGzip()) })
-  //   .send(function(err, data) { process.exit(0) });
-
-  let load = new (require('./restore'))('staging');
-  load.on('data', function(data){
-    console.log(data.data._id)
-  });
-  load.on('end', function(){
-    process.exit(0);
-  });
-
-});
+// series([
+//   // connect
+//   (callback) => {
+//     return connect(url, callback);
+//   },
+//   (callback) => {
+//     return connect.addConnection(local, 'local', callback);
+//   },
+//   (callback) => {
+//     return connect.addConnection(staging, 'staging', callback);
+//   }
+// ], (err, results) => {
+//
+//   let backup = new (require('./dump'))('rezflow');
+//   backup.on('end', function(){
+//     console.log('done writing');
+//   });
+//   // s3.upload({ Body: backup.pipe(zlib.createGzip()) })
+//   //   .send(function(err, data) { process.exit(0) });
+//
+//   let load = new (require('./restore'))('staging');
+//   load.on('data', function(data){
+//     console.log(data.data._id)
+//   });
+//   load.on('end', function(){
+//     process.exit(0);
+//   });
+//
+// });
