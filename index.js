5709422e87810d05b277923bbb73c454b30235bf
Josey Morton <me@josey.io>
2016-09-13 13:21:43 -0500

    removing garbage instantiation

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ global.LOG_LEVEL = argv.verbose || parseInt(process.env.LOG_LEVEL);
 global.DEBUG = argv.debug || parseBoolean(process.env.DEBUG);
 global.log = require('./logging.js');
 
-new (require('./restore'))().insert();
-
 module.exports = {
   export: require('./dump'),
   import: require('./restore')
